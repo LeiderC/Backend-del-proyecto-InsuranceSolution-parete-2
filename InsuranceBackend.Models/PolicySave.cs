e2059db9c2e2bae4dfd1e04e404bc20147f6cbed
Fabian Andrés Alvarez Hoyos <fabian.alvarez@arkangroupco.com>
2019-11-15 16:55:20 -0500

    cambio pendiente

--- InsuranceBackend.Models/PolicySave.cs
+++ InsuranceBackend.Models/PolicySave.cs
@@ -9,5 +9,7 @@ namespace InsuranceBackend.Models
         public Policy Policy { get; set; }
         public List<PolicyProduct> PolicyProducts { get; set; }
         public Vehicle Vehicle { get; set; }
+        public List<Customer> PolicyInsured { get; set; }
+        public List<Beneficiary> PolicyBeneficiaries { get; set; }
     }
 }
